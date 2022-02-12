ba821184745ac52f3ebfc9e19df3cad9f5ce0393
Kelli <kellistone217@gmail.com>
2022-02-11 23:19:11 -0700

    Added route to list all of the names on the nameslist

--- app.js
+++ app.js
@@ -1,14 +1,36 @@
 const express = require('express')
+const es6Renderer = require('express-es6-template-engine')
+const nameslist = require('./routes/nameslist')
+
 
 const app = express()
-const port = 3000;
+const port = "3000"
 
+const db = require('./models/db')
 
-app.get('/', (req, res) => {
-    res.send("Sup")
+const indexRouter = require('./routes/index')
+// const namesList = require('./routes/nameslist')
+
+app.get('/', (req, res) =>{
+    res.json(db);
 })
 
+app.engine('html', es6Renderer);
+app.set('views', 'templates');
+app.set('view engine', 'html');
 
+app.use(express.static('public'))
+
+// app.use("/", indexRouter)
+// app.use("/nameslist", namesList)
+
+// app.get("/", (req, res) => { 
+//     res.render('home', {
+//         partials: {
+//             head: "/partials/home" 
+//         }
+//     });     
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
